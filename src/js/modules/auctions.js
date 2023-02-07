79acd71b7de4675d3e2af20c184220d41efbd96a
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-07 20:54:11 +0600

    working with slider-swiper

--- src/js/modules/auctions.js
+++ src/js/modules/auctions.js
@@ -1,9 +1,20 @@
-import Swiper from "swiper";
+import Swiper from "swiper/bundle";
+// import {
+// 	Navigation,
+// 	EffectFade,
+// 	Pagination,
+// 	Thumbs,
+// 	Controller,
+// 	FreeMode,
+// 	Scrollbar,
+// 	A11y,
+// 	Mousewheel
+// } from "swiper";
 
 export const sliderAuctions = () => {
 	return new Swiper('.swiper', {
 
-		slidesPerView: 1,
+		slidesPerView: 3,
 		spaceBetween: 10,
 
 		navigation: {
