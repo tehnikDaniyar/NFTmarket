17dc14097da0ada882aaf62596b7b45f7186ed7b
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-12 16:37:28 +0600

    end block 'auctions' with adaptive

--- src/js/modules/auctions.js
+++ src/js/modules/auctions.js
@@ -16,13 +16,22 @@ export const sliderAuctions = () => {
 
 		loop: true,
 
-		slidesPerView: 3,
+		slidesPerView: 1,
 		spaceBetween: 10,
 
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
+
+		breakpoints: {
+			676: {
+				slidesPerView: 2,
+			},
+			1100: {
+				slidesPerView: 3,
+			},
+		}
 
 	});
 };
